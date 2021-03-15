1490cc79bfbe79bfee18846014bf9b1fc0b7badf
Yonghwi Kim <yh0921k@gmail.com>
2021-03-15 15:51:38 +0900

    [init] TPay Project 초기화
    module
    - SpringBoot   : 2.4.3
    - Database     : MariaDB, H2
    - Build Tool   : Gradle
    - Language     : Java 11
    
    start.spring.io를 이용한 프로젝트 생성
    - Dependencies : Spring Web, Spring Data JPA,
                   : Lombok, H2 Database, MariaDB Driver
    
    .gitignore 추적 제외 파일 설정
    - IntelliJ, Java, Database, VisualStudioCode
    - Git, Gradle, Eclipse
    - macOS, Windows, Linux

---
+++ src/main/java/com/tpay/TpayServerApplication.java
@@ -0,0 +1,13 @@
+package com.tpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpayServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tpay/TpayServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpayServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
