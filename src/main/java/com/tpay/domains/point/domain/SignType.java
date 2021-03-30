daa00525cc3cc981c7f99b6e5bc30b4c72746900
Yonghwi Kim <yh0921k@gmail.com>
2021-03-30 16:28:21 +0900

    [add] Point 도메인 관련 클래스 구현
    PointEntity.java
    PointRepository.java
    - 해당 엔티티는 변화 금액과 부호, 상태와 잔액을 필드로 가짐
    - FranchiseeEntity와는 다대일 관계로, franchiseeEntity의 주키를 외래키로 가짐
    
    SignType.java
    - 포인트 변화 금액이 양수인지 음수인지 판단할 enum 클래스
    
    PointStatus.java
    - 포인트 상태를 지닐 클래스로 적립, 취소, 출금의 상태를 지님

---
+++ src/main/java/com/tpay/domains/point/domain/SignType.java
@@ -0,0 +1,15 @@
+package com.tpay.domains.point.domain;
+
+public enum SignType {
+  POSITIVE("+"), NEGATIVE("-");
+
+  private final String type;
+
+  SignType(String type) {
+    this.type = type;
+  }
+
+  public String getType() {
+    return this.type;
+  }
+}
