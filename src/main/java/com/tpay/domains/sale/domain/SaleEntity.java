f2be9d94d479688bc2994ecc98972acfd1e36dd5
aejeong <aejeong-context@kakao.com>
2021-04-01 14:37:18 +0900

    [mod] Sale 도메인 관련 클래스 구현
    SaleEntity.java
    - 환급 설계 테이블과 맞춰 길이 설정
    
    SaleRepositoryTest.java
    - 판매자, 주문번호 생성 테스트 추가

--- src/main/java/com/tpay/domains/sale/domain/SaleEntity.java
+++ src/main/java/com/tpay/domains/sale/domain/SaleEntity.java
@@ -1,6 +1,7 @@
 package com.tpay.domains.sale.domain;
 
 import com.tpay.domains.customer.domain.CustomerEntity;
+import com.tpay.domains.franchisee.domain.FranchiseeEntity;
 import lombok.*;
 
 import javax.persistence.*;
@@ -15,24 +16,28 @@ public class SaleEntity {
   @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
-  @Column(name = "saleDate", length = 30)
+  @Column(name = "saleDatm", length = 14, nullable = false)
   private String saleDate;
 
-  @Column(name = "totVat", length = 30)
+  @Column(name = "totVat", length = 12)
   private String totalVat;
 
-  @Column(name = "totAmt", length = 50)
+  @Column(name = "totAmt", length = 12)
   private String totalAmount;
 
-  @Column(name = "totQty", length = 30)
+  @Column(name = "totQty", length = 7)
   private String totalQuantity;
 
-  @Column(name = "purchsSn", length = 50)
+  @Column(name = "purchsSn", length = 20, nullable = false)
   private String orderNumber;
 
   @OneToOne
-  @JoinColumn(name = "customer_id")
+  @JoinColumn(name = "customer_id", nullable = false)
   private CustomerEntity customerEntity;
+
+  @ManyToOne
+  @JoinColumn(name = "franchisee_id", nullable = false)
+  private FranchiseeEntity franchiseeEntity;
 
   @Builder
   public SaleEntity(
@@ -41,12 +46,14 @@ public class SaleEntity {
       String totalAmount,
       String totalQuantity,
       String orderNumber,
-      CustomerEntity customerEntity) {
+      CustomerEntity customerEntity,
+      FranchiseeEntity franchiseeEntity) {
     this.saleDate = saleDate;
     this.totalVat = totalVat;
     this.totalAmount = totalAmount;
     this.totalQuantity = totalQuantity;
     this.orderNumber = orderNumber;
     this.customerEntity = customerEntity;
+    this.franchiseeEntity = franchiseeEntity;
   }
 }
--- src/test/java/com/tpay/domains/sale/domain/SaleRepositoryTest.java
+++ src/test/java/com/tpay/domains/sale/domain/SaleRepositoryTest.java
@@ -1,6 +1,10 @@
 package com.tpay.domains.sale.domain;
 
 import com.tpay.domains.customer.domain.CustomerEntity;
+import com.tpay.domains.customer.domain.CustomerRepository;
+import com.tpay.domains.franchisee.domain.FranchiseeEntity;
+import com.tpay.domains.franchisee.domain.FranchiseeRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -8,7 +12,10 @@ import org.springframework.test.context.ActiveProfiles;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Random;
 
+import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
@@ -17,15 +24,59 @@ import static org.hamcrest.MatcherAssert.assertThat;
 class SaleRepositoryTest {
 
   @Autowired SaleRepository saleRepository;
+  @Autowired FranchiseeRepository franchiseeRepository;
+  @Autowired CustomerRepository customerRepository;
+
+  CustomerEntity customerEntity;
+  FranchiseeEntity franchiseeEntity;
+    String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyyMMddHHmmss"));
+  String businessNumber = ("123-45-67890").replace("-","");
+
+  @BeforeEach
+  public void setup() {
+    franchiseeEntity =
+        franchiseeRepository.save(
+            FranchiseeEntity.builder().businessNumber(businessNumber).build());
+    customerEntity =
+        customerRepository.save(
+            CustomerEntity.builder()
+                .customerName("success")
+                .nation("ko")
+                .passportNumber("3489384")
+                .build());
+  }
 
   @Test
-  public void 판매_생성_테스트() throws Exception {
+  public void 주문_번호_생성_테스트() {
 
-    CustomerEntity customerEntity = CustomerEntity.builder().customerName("success").nation("ko").passportNumber("3489384").build();
+    // given
+    Random random = new Random();
+    int iValue = (random.nextInt(99));
+    String orderNumber = iValue + now;
 
-    String now = LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+    // when
+    SaleEntity saleEntity =
+        SaleEntity.builder()
+            .saleDate(now)
+            .orderNumber(orderNumber)
+            .totalAmount("129000")
+            .totalQuantity("1")
+            .totalVat("14190")
+            .customerEntity(customerEntity)
+            .franchiseeEntity(franchiseeEntity)
+            .build();
+
+    // then
+    System.out.println("TEST orderNumber : " + orderNumber);
+    assertThat(saleEntity.getOrderNumber(), is(equalTo(orderNumber)));
+  }
+
+  @Test
+  public void 판매_생성_테스트() {
+
     // given
     SaleEntity saleEntity =
+            saleRepository.save(
         SaleEntity.builder()
             .saleDate(now)
             .orderNumber("1932487633")
@@ -33,12 +84,13 @@ class SaleRepositoryTest {
             .totalQuantity("1")
             .totalVat("14190")
             .customerEntity(customerEntity)
-            .build();
+            .franchiseeEntity(franchiseeEntity)
+            .build());
 
     // when
-    saleRepository.save(saleEntity);
+    List<SaleEntity> saleEntityList = saleRepository.findAll();
 
     // then
-    assertThat(saleRepository.getOne(saleEntity.getId()), is(saleEntity));
+    assertThat(saleEntityList.get(0), is(equalTo(saleEntity)));
   }
 }
