ea16919cf0c1c7d5fa134e11c4e0e0e5541e67ad
Yonghwi Kim <yh0921k@gmail.com>
2021-04-05 17:56:37 +0900

    [add] 카테고리 리스트를 조회하는 API 구현
    CategoryFindController.java
    CategoryFindService.java
    - 현재 데이터베이스에 저장된 카테고리 리스트를 반환
    - CategoryResponse를 리스트 형태로 반환
    
    CategoryResponse.java
    - 단일 카테고리 데이터를 반환하기 위한 DTO
    
    CategoryRepositoryTest.java
    - CategoryRepository에서 현재 모든 카테고리를 조회하여 기존에 주입해둔 개수와 이름을 비교하는 테스트 코드 작성

--- src/test/java/com/tpay/domains/category/domain/CategoryRepositoryTest.java
+++ src/test/java/com/tpay/domains/category/domain/CategoryRepositoryTest.java
@@ -4,7 +4,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
-import org.junit.jupiter.api.BeforeEach;
+import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -21,10 +21,21 @@ class CategoryRepositoryTest {
     // given
 
     // when
-    CategoryEntity savedCategoryEntity =
-        categoryRepository.findAll().stream().findFirst().get();
+    CategoryEntity savedCategoryEntity = categoryRepository.findAll().stream().findFirst().get();
 
     // then
     assertThat(savedCategoryEntity.getName(), is(equalTo("잡화")));
+  }
+
+  @Test
+  public void 카테고리_리스트_조회_테스트() {
+    // given
+
+    // when
+    List<CategoryEntity> categoryEntityList = categoryRepository.findAll();
+
+    // then
+    assertThat(categoryEntityList.size(), is(6));
+    assertThat(categoryEntityList.get(0).getName(), is("잡화"));
   }
 }
