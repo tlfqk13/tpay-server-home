1a8b336c66e7c5c8d9e387917936c8e25deb9f71
Yonghwi Kim <yh0921k@gmail.com>
2021-04-07 10:40:47 +0900

    [mod] 본인인증 로직 변경 및 CSRF 처리 로직 추가
    SecurityConfig.java
    - 기본적인 Security 설정 이후 401 반환을 막기위한 처리
    
    SMSCertificateController.java
    - RequestParam -> PathVariable로 파라미터 전달 변경

--- src/main/java/com/tpay/commons/security/SecurityConfig.java
+++ src/main/java/com/tpay/commons/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.tpay.commons.security;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -8,6 +9,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable().authorizeRequests().antMatchers("/**").permitAll();
+  }
+
   @Bean
   public PasswordEncoder passwordEncoder() {
     return new BCryptPasswordEncoder(4);
