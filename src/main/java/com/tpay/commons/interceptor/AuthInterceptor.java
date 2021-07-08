73d4e07c55b5d48f38688e55907e55c7787f726a
Yonghwi Kim <yh0921k@gmail.com>
2021-07-08 16:33:33 +0900

    [add] JWT 인증 관련 도메인 로직 구현
    application.yml
    - JWT 서명에 필요한 키 문자열 추가
    
    AuthToken.java
    - JWT 토큰 클래스로, 해당 토큰의 문자열과 key 필드로 구성
    - payload를 전달받는 생성자는 초기 토큰 생성시에 사용
    - value를 전달받는 생성자는 클라이언트의 토큰 문자열을 전달받아 유효성 검사 및 데이터 파싱등을 처리하기 위한 변환에 쓰임
    - 이외에 토큰 생성, 토큰 파싱, 헤더 생성, 유효성 검사등의 로직 구현
    
    JwtUtils.java
    - 토큰과 관련된 기능을 처리할 유틸리티 클래스로, 초기 생성시 Secret Key를 초기화하여 지님
    - 토큰 생성, 토큰 변환, 만료시간 생성, 공통 데이터 추가 등의 로직 구현
    
    TokenType.java
    - 토큰 타입은 Access Token과 Refresh Token으로 구성
    - 각 만료 시간은 하루, 일주일이며 토큰 이름을 사용해 만료시간을 얻는 메서드 구현
    
    WebMvcConfig.java
    - 토큰 검증 인터셉터를 설정
    - 로그인, 로그아웃, 토큰 재발급의 케이스를 제외한 모든 요청에 등록
    
    AuthInterceptor.java
    - 토큰 검증을 위한 인터셉터로, Http Header에서 토큰을 파싱해 유효성 검증

---
+++ src/main/java/com/tpay/commons/interceptor/AuthInterceptor.java
@@ -0,0 +1,48 @@
+package com.tpay.commons.interceptor;
+
+import com.tpay.commons.exception.ExceptionState;
+import com.tpay.commons.exception.detail.FranchiseeAuthenticationException;
+import com.tpay.commons.jwt.AuthToken;
+import com.tpay.commons.jwt.JwtUtils;
+import java.util.Optional;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class AuthInterceptor implements HandlerInterceptor {
+
+  private final JwtUtils jwtUtils;
+
+  @Override
+  public boolean preHandle(
+      HttpServletRequest servletRequest, HttpServletResponse servletResponse, Object handler)
+      throws Exception {
+
+    Optional<String> token = parseToken(servletRequest);
+    if (token.isEmpty()) {
+      throw new FranchiseeAuthenticationException(
+          ExceptionState.AUTHENTICATION_FAILED, "Token not exists");
+    }
+
+    AuthToken authToken = jwtUtils.convertAuthToken(token.get());
+    authToken.validate();
+    return true;
+  }
+
+  private Optional<String> parseToken(HttpServletRequest request) {
+    String authToken = request.getHeader(HttpHeaders.AUTHORIZATION);
+    if (StringUtils.hasText(authToken)) {
+      return Optional.of(authToken);
+    } else {
+      return Optional.empty();
+    }
+  }
+}
