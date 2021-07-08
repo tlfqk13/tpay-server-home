9011fb31c2f20e8e8d65a0be86df5647c358453d
Yonghwi Kim <yh0921k@gmail.com>
2021-07-08 15:37:13 +0900

    [mod] 사업자등록번호로 엔티티를 조회하는 비즈니스 로직 분리
    FranchiseeFindService.java
    - 사업자 등록번호를 사용하여 가맹점 엔티티를 조회하는 로직 구현
    
    PasswordResetService.java
    - 위에서 분리한 기능을 사용하여 가맹점 엔티티를 조회하는 로직으로 변경

--- src/main/java/com/tpay/domains/franchisee/application/PasswordResetService.java
+++ src/main/java/com/tpay/domains/franchisee/application/PasswordResetService.java
@@ -2,7 +2,6 @@ package com.tpay.domains.franchisee.application;
 
 import com.tpay.domains.franchisee.application.dto.PasswordResetRequest;
 import com.tpay.domains.franchisee.domain.FranchiseeEntity;
-import com.tpay.domains.franchisee.domain.FranchiseeRepository;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -13,15 +12,13 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class PasswordResetService {
 
-  private final FranchiseeRepository franchiseeRepository;
+  private final FranchiseeFindService franchiseeFindService;
   private final PasswordEncoder passwordEncoder;
 
   @Transactional
   public ResponseEntity resetPassword(PasswordResetRequest passwordResetRequest) {
     FranchiseeEntity franchiseeEntity =
-        franchiseeRepository
-            .findByBusinessNumber(passwordResetRequest.getBusinessNumber())
-            .orElseThrow(() -> new IllegalArgumentException("Invalid Business Number"));
+        franchiseeFindService.findByBusinessNumber(passwordResetRequest.getBusinessNumber());
 
     franchiseeEntity.resetPassword(passwordEncoder.encode(passwordResetRequest.getPassword()));
     return ResponseEntity.ok().build();
