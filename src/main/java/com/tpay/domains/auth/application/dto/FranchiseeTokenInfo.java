6e610d0ad0f93528f8241e05522a834900d4ea69
Yonghwi Kim <yh0921k@gmail.com>
2021-09-16 15:08:05 +0900

    [mod] 가맹점 로그인 필드 추가 반환
    FranchiseeSignInService.java
    FranchiseeTokenInfo.java
    - 사업자등록번호 필드 추가 반환

--- src/main/java/com/tpay/domains/auth/application/dto/FranchiseeTokenInfo.java
+++ src/main/java/com/tpay/domains/auth/application/dto/FranchiseeTokenInfo.java
@@ -13,6 +13,7 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class FranchiseeTokenInfo {
   private Long franchiseeIndex;
+  private String businessNumber;
   private FranchiseeStatus franchiseeStatus;
   private String rejectReason;
   private String accessToken;
