a225aebdb64c1817c3efa8ddf1f3625237908e36
Yonghwi Kim <yh0921k@gmail.com>
2021-09-23 11:32:03 +0900

    [add] 상품엔티티 조회 쿼리 메서드 구현
    ProductRepository.java
    - 이름과 가격 필드로 상품 엔티티를 조회하는 쿼리 메서드 구현

--- src/main/java/com/tpay/domains/product/domain/ProductRepository.java
+++ src/main/java/com/tpay/domains/product/domain/ProductRepository.java
@@ -1,5 +1,8 @@
 package com.tpay.domains.product.domain;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductRepository extends JpaRepository<ProductEntity, Long> {}
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+  Optional<ProductEntity> findByNameAndPrice(String name, String price);
+}
