e75360d04b28a8ad3c498d367f6812f604461531
Yonghwi Kim <yh0921k@gmail.com>
2021-09-24 12:00:05 +0900

    [mod] 주문 엔티티 저장 로직 변경
    OrderSaveService.java
    - 사용자의 환급승인요청 DTO를 사용하여 가맹점, 관광객, 상품 엔티티를 조회
    - 이후 주문 엔티티 및 주문건 엔티티를 생성 및 영속화하며 연관관계 매핑 수행

--- src/main/java/com/tpay/domains/order/application/OrderSaveService.java
+++ src/main/java/com/tpay/domains/order/application/OrderSaveService.java
@@ -1,10 +1,14 @@
 package com.tpay.domains.order.application;
 
+import com.tpay.domains.customer.application.CustomerFindService;
 import com.tpay.domains.customer.domain.CustomerEntity;
+import com.tpay.domains.franchisee.application.FranchiseeFindService;
 import com.tpay.domains.franchisee.domain.FranchiseeEntity;
 import com.tpay.domains.order.domain.OrderEntity;
 import com.tpay.domains.order.domain.OrderRepository;
+import com.tpay.domains.product.application.ProductFindService;
 import com.tpay.domains.product.domain.ProductEntity;
+import com.tpay.domains.refund.application.dto.RefundSaveRequest;
 import javax.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -14,7 +18,24 @@ import org.springframework.stereotype.Service;
 public class OrderSaveService {
 
   private final OrderRepository orderRepository;
+  private final FranchiseeFindService franchiseeFindService;
+  private final CustomerFindService customerFindService;
+  private final ProductFindService productFindService;
   private final OrderLineSaveService orderLineSaveService;
+
+  @Transactional
+  public OrderEntity save(RefundSaveRequest request) {
+    FranchiseeEntity franchiseeEntity =
+        franchiseeFindService.findByIndex(request.getFranchiseeIndex());
+
+    CustomerEntity customerEntity = customerFindService.findByIndex(request.getCustomerIndex());
+
+    ProductEntity productEntity =
+        productFindService.findOrElseSave(
+            franchiseeEntity.getProductCategory(), request.getPrice());
+
+    return this.save(franchiseeEntity, customerEntity, productEntity);
+  }
 
   @Transactional
   public OrderEntity save(
@@ -28,6 +49,7 @@ public class OrderSaveService {
             .customerEntity(customerEntity)
             .build();
 
+    orderRepository.save(orderEntity);
     orderLineSaveService.save(orderEntity, productEntity);
 
     return orderEntity;
