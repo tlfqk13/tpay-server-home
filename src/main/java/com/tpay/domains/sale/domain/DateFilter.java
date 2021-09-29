bdc86f4f71e368903e144ff15dd2e5b1224ed61f
Yonghwi Kim <yh0921k@gmail.com>
2021-09-29 09:00:16 +0900

    [add] 날짜 필터에 따른 enum 클래스 구현
    DateFilter.java
    - 각 필드는 당일, 일주일, 한달, 사용자 기간조회로 구성됨

---
+++ src/main/java/com/tpay/domains/sale/domain/DateFilter.java
@@ -0,0 +1,22 @@
+package com.tpay.domains.sale.domain;
+
+import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public enum DateFilter {
+  TODAY(LocalDate.now(), LocalDate.now().plusDays(1)),
+  WEEK(LocalDate.now().minusDays(7), LocalDate.now().plusDays(1)),
+  MONTH(LocalDate.now().minusMonths(1), LocalDate.now().plusDays(1)),
+  CUSTOM;
+
+  private LocalDate startDate;
+  private LocalDate endDate;
+
+  DateFilter(LocalDate startDate, LocalDate endDate) {
+    this.startDate = startDate;
+    this.endDate = endDate;
+  }
+}
