d319fd448a931aebd1054d8c439cfeb0854630f2
Yonghwi Kim <yh0921k@gmail.com>
2021-11-16 16:04:36 +0900

    mod: 관광객 엔티티 저장 로직 수정
    CustomerSaveService.java
    - 여권번호는 암호화하여 저장

--- src/main/java/com/tpay/domains/customer/application/CustomerSaveService.java
+++ src/main/java/com/tpay/domains/customer/application/CustomerSaveService.java
@@ -1,5 +1,6 @@
 package com.tpay.domains.customer.application;
 
+import com.tpay.commons.aria.PassportNumberEncryptService;
 import com.tpay.domains.customer.domain.CustomerEntity;
 import com.tpay.domains.customer.domain.CustomerRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,15 +10,18 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class CustomerSaveService {
 
+  private final PassportNumberEncryptService passportNumberEncryptService;
   private final CustomerRepository customerRepository;
 
   public CustomerEntity saveByCustomerInfo(
       String customerName, String passportNumber, String nationality) {
 
+    String encryptedPassportNumber = passportNumberEncryptService.encrypt(passportNumber);
+
     CustomerEntity customerEntity =
         CustomerEntity.builder()
             .customerName(customerName)
-            .passportNumber(passportNumber)
+            .passportNumber(encryptedPassportNumber)
             .nation(nationality)
             .build();
 
