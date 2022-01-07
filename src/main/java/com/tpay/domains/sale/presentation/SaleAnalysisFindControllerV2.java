9f76ff4a167fc5d57301d76415bbb6b6e016ee99
sunba <nsg2318@naver.com>
2022-01-07 09:54:47 +0900

    add : 판매실적 조회 지속적인 에러에따라 모든 파라미터 String으로 받는 V2 생성

---
+++ src/main/java/com/tpay/domains/sale/presentation/SaleAnalysisFindControllerV2.java
@@ -0,0 +1,30 @@
+package com.tpay.domains.sale.presentation;
+
+
+import com.tpay.commons.util.DateFilterV2;
+import com.tpay.domains.sale.application.SaleAnalysisFindServiceV2;
+import com.tpay.domains.sale.application.dto.SaleAnalysisFindResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class SaleAnalysisFindControllerV2 {
+
+  private final SaleAnalysisFindServiceV2 saleAnalysisFindServiceV2;
+
+  @GetMapping("/sales/franchiseeV2/{franchiseeIndex}")
+  public ResponseEntity<List<SaleAnalysisFindResponse>> findByDateRange(
+      @PathVariable Long franchiseeIndex,
+      @RequestParam DateFilterV2 dateFilter
+      ) {
+    List<SaleAnalysisFindResponse> result = saleAnalysisFindServiceV2.findByDateRange(franchiseeIndex, dateFilter);
+    return ResponseEntity.ok(result);
+  }
+}
