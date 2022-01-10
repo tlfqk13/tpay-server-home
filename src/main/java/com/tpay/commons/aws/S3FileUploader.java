3a717c368cee95462bff2c52ea3c12ad171800aa
sunba <nsg2318@naver.com>
2022-01-10 21:03:50 +0900

    add : S3FileUploader 추가- 2

--- src/main/java/com/tpay/commons/aws/S3FileUploader.java
+++ src/main/java/com/tpay/commons/aws/S3FileUploader.java
@@ -6,11 +6,15 @@ import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.PutObjectRequest;
 import lombok.NoArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
 
 @Service
 @NoArgsConstructor
@@ -40,7 +44,10 @@ public class S3FileUploader {
         .build();
   }
 
-
-
+  public String upload(MultipartFile file) throws IOException {
+    String filename = file.getOriginalFilename();
+    s3Client.putObject(new PutObjectRequest(bucket,filename,file.getInputStream(),null).withCannedAcl(CannedAccessControlList.PublicRead));
+    return s3Client.getUrl(bucket,filename).toString();
+  }
 
 }
