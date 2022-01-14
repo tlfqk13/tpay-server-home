4ea806b7f6f8d8dde43ef269346add99d656e937
sunba <nsg2318@naver.com>
2022-01-14 11:59:33 +0900

    add : 사업자번호 유효, 은행 정보 유효 API 키 등록

--- src/main/java/com/tpay/commons/custom/CustomValue.java
+++ src/main/java/com/tpay/commons/custom/CustomValue.java
@@ -10,6 +10,9 @@ public class CustomValue {
   public static String IAMPORT_SECRET;
   public static String REFUND_SERVER;
   public static String APPLICATION_CODE;
+  public static String BUSINESS_VALID_ACCESS;
+  public static String BANK_VALID_ID;
+  public static String BANK_VALID_SECRET;
 
   @Value("${custom.iamport.api-key}")
   public void setIamportApiKey(String iamportApiKey) {
@@ -30,4 +33,13 @@ public class CustomValue {
   public void setApplicationCode(String applicationCode) {
     APPLICATION_CODE = applicationCode;
   }
+
+  @Value("${custom.upload.business}")
+  public void setBusinessValidAccess(String businessValidAccess) { BUSINESS_VALID_ACCESS = businessValidAccess;}
+
+  @Value("${custom.upload.bank.client-id}")
+  public void setBankValidId(String bankValidId) {BANK_VALID_ID = bankValidId;}
+
+  @Value("${custom.upload.bank.client-secret}")
+  public void setBankValidSecret(String bankValidSecret) {BANK_VALID_SECRET = bankValidSecret;}
 }
---
+++ src/main/java/com/tpay/domains/franchisee_upload/presentation/UploadValidationController.java
@@ -0,0 +1,5 @@
+package com.tpay.domains.franchisee_upload.presentation;
+
+
+public class UploadValidationController {
+}
