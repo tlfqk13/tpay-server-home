1e96f867703607d7711759b94ea83c6ed20b188f
sunba <nsg2318@naver.com>
2022-01-18 11:27:53 +0900

    mod : 은행정보 업로드 업데이트

--- src/main/java/com/tpay/domains/franchisee_upload/application/FranchiseeBankService.java
+++ src/main/java/com/tpay/domains/franchisee_upload/application/FranchiseeBankService.java
@@ -1,6 +1,8 @@
 package com.tpay.domains.franchisee_upload.application;
 
 
+import com.tpay.domains.franchisee.application.FranchiseeFindService;
+import com.tpay.domains.franchisee.domain.FranchiseeEntity;
 import com.tpay.domains.franchisee_upload.application.dto.FranchiseeBankInfo;
 import com.tpay.domains.franchisee_upload.domain.FranchiseeBankEntity;
 import com.tpay.domains.franchisee_upload.domain.FranchiseeBankRepository;
@@ -12,12 +14,16 @@ import org.springframework.stereotype.Service;
 public class FranchiseeBankService {
 
   private final FranchiseeBankRepository franchiseeBankRepository;
+  private final FranchiseeFindService franchiseeFindService;
 
-  public FranchiseeBankInfo save(FranchiseeBankInfo franchiseeBankInfo) {
+  public FranchiseeBankInfo save(Long franchiseeIndex, FranchiseeBankInfo franchiseeBankInfo) {
+
+    FranchiseeEntity franchiseeEntity = franchiseeFindService.findByIndex(franchiseeIndex);
     FranchiseeBankEntity franchiseeBankEntity = FranchiseeBankEntity.builder()
-        .accountNumber(franchiseeBankInfo.getAccountNumber())
+        .accountNumber(franchiseeBankInfo.getAccountNumber().replaceAll("-",""))
         .bankName(franchiseeBankInfo.getBankName())
         .withdrawalDate(franchiseeBankInfo.getWithdrawalDate())
+        .franchiseeEntity(franchiseeEntity)
         .build();
     // TODO: 2022/01/12 은행컬럼들의 각 필요이유에 따라 설계 수정될 가능성 있음
     FranchiseeBankEntity result = franchiseeBankRepository.save(franchiseeBankEntity);
