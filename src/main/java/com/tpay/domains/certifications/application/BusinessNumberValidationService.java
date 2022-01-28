26c82ad75b7f83e5fd0fa444899a310992bc1ae9
sunba <nsg2318@naver.com>
2022-01-28 10:32:12 +0900

    mod : 테스트코드 실패건 수정 및 로직일부 수정

--- src/main/java/com/tpay/domains/certifications/application/BusinessNumberValidationService.java
+++ src/main/java/com/tpay/domains/certifications/application/BusinessNumberValidationService.java
@@ -14,7 +14,7 @@ public class BusinessNumberValidationService {
   private final FranchiseeRepository franchiseeRepository;
 
   public void valid(Long businessNumber) {
-    if (franchiseeRepository.existsByBusinessNumber(businessNumber.toString())) {
+    if (franchiseeRepository.existsByBusinessNumber(businessNumber.toString().replaceAll("-",""))) {
       throw new AlreadyExistsException(ExceptionState.ALREADY_EXISTS, "Franchisee Already Exists");
     }
   }
