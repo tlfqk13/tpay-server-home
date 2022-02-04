0938c9c254ce0fec448094f24ac7003e5098c901
sunba <nsg2318@naver.com>
2022-02-04 14:16:44 +0900

    add : xlsx S3 업로드 완료

--- src/main/java/com/tpay/domains/franchisee/application/FranchiseeCmsService.java
+++ src/main/java/com/tpay/domains/franchisee/application/FranchiseeCmsService.java
@@ -1,6 +1,7 @@
 package com.tpay.domains.franchisee.application;
 
 
+import com.tpay.commons.aws.S3FileUploader;
 import com.tpay.commons.exception.ExceptionState;
 import com.tpay.commons.exception.detail.InvalidParameterException;
 import com.tpay.domains.franchisee.application.dto.cms.FranchiseeCmsResponseDetailInterface;
@@ -15,7 +16,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileOutputStream;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -26,6 +27,7 @@ import static org.apache.poi.ss.usermodel.CellType.STRING;
 public class FranchiseeCmsService {
 
   private final OrderRepository orderRepository;
+  private final S3FileUploader s3FileUploader;
 
   public FranchiseeCmsResponseInterface cmsReport(Long franchiseeIndex, String requestDate) {
     List<String> date = setUpDate(requestDate);
@@ -48,19 +50,30 @@ public class FranchiseeCmsService {
       FileInputStream fileInputStream = new FileInputStream(file);
       XSSFWorkbook xssfWorkbook = new XSSFWorkbook(fileInputStream);
       XSSFSheet sheet = xssfWorkbook.getSheetAt(0);
-      XSSFRow row = sheet.createRow(0);
-      row.createCell(0, STRING);
-      row.getCell(0).setCellValue("0번 cellNum에 입력");
+      XSSFRow row = sheet.createRow(12);
+      XSSFRow row1 = sheet.getRow(11);
+      row.createCell(11, STRING);
+      row.createCell(5, STRING);
+      row1.getCell(5).setCellValue("수신자만 바뀌었으면 성공");
+      row.getCell(5).setCellValue("기존것도 바뀌는지 테스트"); //실패
+      row.getCell(11).setCellValue("가맹점번호 : " + franchiseeIndex + "가 입력되었습니다.");
       // TODO: 2022/02/03 엑셀파일 일부 write 후 저장까지 테스트 완료 포멧에 맞게 입력하는 로직 구현할 것
-      FileOutputStream fileOutputStream = new FileOutputStream("/Users/sunba/excelExportTest.xlsx", false);
-      xssfWorkbook.write(fileOutputStream);
-      fileOutputStream.close();
-    } catch (Exception e) {
+//      FileOutputStream fileOutputStream = new FileOutputStream("/Users/sunba/excelExportTest.xlsx", false);
+//      xssfWorkbook.write(fileOutputStream);
+//      fileOutputStream.flush();
+//      fileOutputStream.close();
+
+      System.out.println("아웃풋스트림 생성 전");
+      String result = s3FileUploader.uploadXlsx(franchiseeIndex, xssfWorkbook);
+      return result;
+
+
+    } catch (IOException e) {
       throw new InvalidParameterException(ExceptionState.INVALID_PARAMETER, "File Input Failed");
     }
 
-    FranchiseeCmsResponseDetailInterface franchiseeCmsResponseDetailInterface = cmsDetail(franchiseeIndex, requestDate);
-    return "aa";
+//    FranchiseeCmsResponseDetailInterface franchiseeCmsResponseDetailInterface = cmsDetail(franchiseeIndex, requestDate);
+//    return "실패";
   }
 
   public List<String> setUpDate(String requestDate) {
