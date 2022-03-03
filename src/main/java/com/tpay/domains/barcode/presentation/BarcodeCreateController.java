5e0b154afb529b3db632556b0cb8f12ca459b2da
sunba <nsg2318@naver.com>
2022-03-03 16:24:26 +0900

    mod : 직원 삭제 로직 변경

--- src/main/java/com/tpay/domains/barcode/presentation/BarcodeCreateController.java
+++ src/main/java/com/tpay/domains/barcode/presentation/BarcodeCreateController.java
@@ -1,11 +1,13 @@
 package com.tpay.domains.barcode.presentation;
 
 
+import com.tpay.domains.refund_core.application.dto.RefundLimitRequest;
 import lombok.RequiredArgsConstructor;
 import net.sourceforge.barbecue.Barcode;
 import net.sourceforge.barbecue.BarcodeFactory;
 import net.sourceforge.barbecue.BarcodeImageHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.File;
@@ -15,11 +17,14 @@ import java.io.File;
 public class BarcodeCreateController {
 
   @GetMapping("/barcode")
-  public void barcodeMaker() throws Exception {
+  public void barcodeMaker(
+      @RequestBody RefundLimitRequest refundLimitRequest
+      ) throws Exception {
     // TODO: 2022/03/03   여권번호 길이는 고정값으로하면 좋음(바코드 인식 등 오류 최소화)
     // 그러므로 최대 여권번호 길이 기반으로 하면 될 듯
+
     Barcode barcode = BarcodeFactory.createCode128("45670500000");
-    Barcode barcode2 = BarcodeFactory.createCode128("M12345670500000");
+    Barcode barcode2 = BarcodeFactory.createCode128("M12345670 2500000");
     File file = new File("/Users/sunba/Desktop/testPNG.png");
     File file2 = new File("/Users/sunba/Desktop/test2PNG.png");
     BarcodeImageHandler.savePNG(barcode,file);
