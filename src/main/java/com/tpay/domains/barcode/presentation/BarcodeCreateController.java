098079d1da1100a24a5849294674d588fc579f16
sunba <nsg2318@naver.com>
2022-03-03 16:56:56 +0900

    add : 바코드 리퀘스트 Dto 추가

--- src/main/java/com/tpay/domains/barcode/presentation/BarcodeCreateController.java
+++ src/main/java/com/tpay/domains/barcode/presentation/BarcodeCreateController.java
@@ -1,12 +1,14 @@
 package com.tpay.domains.barcode.presentation;
 
 
-import com.tpay.domains.refund_core.application.dto.RefundLimitRequest;
+import com.tpay.commons.exception.ExceptionState;
+import com.tpay.commons.exception.detail.InvalidPassportInfoException;
+import com.tpay.domains.barcode.application.dto.BarcodeCreateRequest;
 import lombok.RequiredArgsConstructor;
 import net.sourceforge.barbecue.Barcode;
 import net.sourceforge.barbecue.BarcodeFactory;
 import net.sourceforge.barbecue.BarcodeImageHandler;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,18 +18,30 @@ import java.io.File;
 @RequiredArgsConstructor
 public class BarcodeCreateController {
 
-  @GetMapping("/barcode")
+  @PostMapping("/barcode")
   public void barcodeMaker(
-      @RequestBody RefundLimitRequest refundLimitRequest
-      ) throws Exception {
+      @RequestBody BarcodeCreateRequest barcodeCreateRequest
+  ) throws Exception {
     // TODO: 2022/03/03   여권번호 길이는 고정값으로하면 좋음(바코드 인식 등 오류 최소화)
     // 그러므로 최대 여권번호 길이 기반으로 하면 될 듯
-
-    Barcode barcode = BarcodeFactory.createCode128("45670500000");
-    Barcode barcode2 = BarcodeFactory.createCode128("M12345670 2500000");
+    String passportNumberWithSpaces = setPassportNumberWithSpaces(barcodeCreateRequest.getPassportNumber());
+    String deduction = barcodeCreateRequest.getDeduction();
+    Barcode barcode = BarcodeFactory.createCode128(passportNumberWithSpaces + deduction);
     File file = new File("/Users/sunba/Desktop/testPNG.png");
-    File file2 = new File("/Users/sunba/Desktop/test2PNG.png");
-    BarcodeImageHandler.savePNG(barcode,file);
-    BarcodeImageHandler.savePNG(barcode2,file2);
+    BarcodeImageHandler.savePNG(barcode, file);
+  }
+
+
+  static String setPassportNumberWithSpaces(String passportNumber) {
+    StringBuilder stringBuilder = new StringBuilder();
+    StringBuilder passportNumberTen = stringBuilder.append(passportNumber);
+    if (passportNumberTen.length() <= 10) {
+      while (passportNumberTen.length() < 10) {
+        passportNumberTen.append(" ");
+      }
+    } else {
+      throw new InvalidPassportInfoException(ExceptionState.INVALID_PASSWORD, "PassportNumber too Long (Max : 10)");
+    }
+    return passportNumberTen.toString();
   }
 }
