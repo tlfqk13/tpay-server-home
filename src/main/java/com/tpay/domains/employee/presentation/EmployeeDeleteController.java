c57f331637d7c35088a7f0964ce50dee70b07149
sunba <nsg2318@naver.com>
2022-03-03 15:50:34 +0900

    mod : 직원 삭제를 리스트 형식으로 변경

--- src/main/java/com/tpay/domains/employee/presentation/EmployeeDeleteController.java
+++ src/main/java/com/tpay/domains/employee/presentation/EmployeeDeleteController.java
@@ -2,10 +2,12 @@ package com.tpay.domains.employee.presentation;
 
 
 import com.tpay.domains.employee.application.EmployeeDeleteService;
+import com.tpay.domains.employee.application.dto.EmployeeDeleteRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,9 +16,9 @@ public class EmployeeDeleteController {
 
   private final EmployeeDeleteService employeeDeleteService;
 
-  @DeleteMapping("/employee/{employeeIndex}")
-  public ResponseEntity<Boolean> delete(@PathVariable Long employeeIndex){
-    employeeDeleteService.delete(employeeIndex);
+  @DeleteMapping("/employee")
+  public ResponseEntity<Boolean> delete(@RequestBody EmployeeDeleteRequest employeeDeleteRequest){
+    employeeDeleteService.delete(employeeDeleteRequest);
     return ResponseEntity.ok(true);
   }
 }
