d75a29fea669d6e86a1affe537c150d23a45f765
sunba <nsg2318@naver.com>
2022-03-10 16:11:56 +0900

    mod : 포인트 조회시 상태 추가

--- src/main/java/com/tpay/domains/point/domain/PointRepository.java
+++ src/main/java/com/tpay/domains/point/domain/PointRepository.java
@@ -14,12 +14,15 @@ public interface PointRepository extends JpaRepository<PointEntity, Long> {
   List<PointEntity> findAllByFranchiseeEntityIdAndCreatedDateBetween(
       Long franchiseeId, LocalDateTime startDate, LocalDateTime endDate, Pageable pageable);
 
-  @Query(value = "select\n" +
-      "    cast(sum(if(created_date > :scheduledDate, change_value,0)) as integer) as scheduledPoint,\n" +
-      "    cast(sum(if(created_date < :disappearDate, change_value,0)) as integer) as disappearPoint,\n" +
-      "    cast(sum(if(created_date between  :disappearDate and :scheduledDate, change_value,0)) as integer) as totalPoint\n" +
-      "    from (select * from points\n" +
-      "          where order_id not in (select distinct order_id from points where point_status = 'CANCEL')) as points\n" +
-      "where franchisee_id = :franchiseeIndex", nativeQuery = true)
+  @Query(value = "select cast(sum(if(created_date > :scheduledDate, change_value, 0)) as integer)                          as scheduledPoint,\n" +
+      "       cast(sum(if(created_date < :disappearDate, change_value, 0)) as integer)                          as disappearPoint,\n" +
+      "       cast(sum(if(created_date between :disappearDate and :scheduledDate, change_value, 0)) as integer) as totalPoint,\n" +
+      "       franchisee_status\n" +
+      "from (select *\n" +
+      "      from points\n" +
+      "      where order_id not in (select distinct order_id from points where point_status = 'CANCEL')) as points\n" +
+      "         left join (select franchisee_id as franchisee_index, franchisee_status from franchisee_applicant) as fa\n" +
+      "                   on points.franchisee_id = fa.franchisee_index\n" +
+      "where franchisee_id = :franchiseeIndex;", nativeQuery = true)
   PointTotalResponseInterface findPointsTotal(@Param("franchiseeIndex") Long franchiseeIndex, @Param("disappearDate") LocalDate disappearDate, @Param("scheduledDate") LocalDate scheduledDate);
 }
