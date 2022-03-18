72c2f3db73cd253d88b82340fc1dc447450f6bbe
sunba <nsg2318@naver.com>
2022-03-18 16:28:05 +0900

    mod : 소멸 정책 변경

--- src/main/java/com/tpay/commons/util/DisappearDate.java
+++ src/main/java/com/tpay/commons/util/DisappearDate.java
@@ -8,10 +8,11 @@ import java.time.LocalDate;
 @Getter
 @NoArgsConstructor
 public enum DisappearDate {
-  DISAPPEAR_DATE(LocalDate.now().minusYears(5));
+  DISAPPEAR_DATE(LocalDate.now().minusYears(5).plusMonths(1).minusDays(1));
 
   private LocalDate disappearDate;
 
+  // TODO: 2022/03/18 -5y+1m-1d 기준보다 작거나 같은 데이터 모두 날려야합니다.
   DisappearDate(LocalDate disappearDate) {
     this.disappearDate = disappearDate;
   }
