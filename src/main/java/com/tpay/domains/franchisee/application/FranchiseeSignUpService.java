b710a23c8170be338c28a133d21681f2b269fc69
sunba <nsg2318@naver.com>
2022-04-14 16:24:57 +0900

    add : 회원가입시 토큰 받아옴

--- src/main/java/com/tpay/domains/franchisee/application/FranchiseeSignUpService.java
+++ src/main/java/com/tpay/domains/franchisee/application/FranchiseeSignUpService.java
@@ -7,10 +7,13 @@ import com.tpay.commons.exception.detail.InvalidParameterException;
 import com.tpay.commons.exception.detail.InvalidPasswordException;
 import com.tpay.commons.regex.RegExType;
 import com.tpay.commons.regex.RegExUtils;
+import com.tpay.commons.util.UserSelector;
 import com.tpay.domains.franchisee.application.dto.FranchiseeSignUpRequest;
 import com.tpay.domains.franchisee.domain.FranchiseeEntity;
 import com.tpay.domains.franchisee.domain.FranchiseeRepository;
 import com.tpay.domains.franchisee_applicant.application.FranchiseeApplicantSaveService;
+import com.tpay.domains.push.application.UserPushTokenService;
+import com.tpay.domains.push.domain.UserPushTokenEntity;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
@@ -25,6 +28,7 @@ public class FranchiseeSignUpService {
     private final PasswordEncoder passwordEncoder;
     private final RegExUtils regExUtils;
     private final FranchiseeApplicantSaveService franchiseeApplicantSaveService;
+    private final UserPushTokenService userPushTokenService;
 
     @Transactional
     public void signUp(FranchiseeSignUpRequest request) {
@@ -32,14 +36,14 @@ public class FranchiseeSignUpService {
         String businessNumber = request.getBusinessNumber();
         if (!regExUtils.validate(RegExType.BUSINESS_NUMBER, businessNumber)) {
             throw new InvalidBusinessNumberException(
-                    ExceptionState.INVALID_BUSINESS_NUMBER,
-                    "Required Business Number Format : (XXX-XX-XXXXX)");
+                ExceptionState.INVALID_BUSINESS_NUMBER,
+                "Required Business Number Format : (XXX-XX-XXXXX)");
         }
 
         String password = request.getPassword();
         if (!regExUtils.validate(RegExType.PASSWORD, password)) {
             throw new InvalidPasswordException(
-                    ExceptionState.INVALID_PASSWORD, "Invalid Password Format");
+                ExceptionState.INVALID_PASSWORD, "Invalid Password Format");
         }
 
         if (franchiseeRepository.existsByBusinessNumber(businessNumber.replaceAll("-", ""))) {
@@ -52,22 +56,29 @@ public class FranchiseeSignUpService {
 
         String encodedPassword = passwordEncoder.encode(password);
         FranchiseeEntity franchiseeEntity =
-                FranchiseeEntity.builder()
-                        .businessNumber(request.getBusinessNumber())
-                        .storeName(request.getStoreName())
-                        .storeAddressNumber(request.getStoreAddressNumber())
-                        .storeAddressBasic(request.getStoreAddressBasic())
-                        .storeAddressDetail(request.getStoreAddressDetail())
-                        .sellerName(request.getSellerName())
-                        .storeTel(request.getStoreTel().replaceAll("-", ""))
-                        .productCategory(request.getProductCategory())
-                        .password(encodedPassword)
-                        .signboard(request.getSignboard())
-                        .storeNumber(request.getStoreNumber().replaceAll("-", ""))
-                        .email(request.getEmail())
-                        .isTaxRefundShop(request.getIsTaxRefundShop())
-                        .build();
-        franchiseeRepository.save(franchiseeEntity);
+            FranchiseeEntity.builder()
+                .businessNumber(request.getBusinessNumber())
+                .storeName(request.getStoreName())
+                .storeAddressNumber(request.getStoreAddressNumber())
+                .storeAddressBasic(request.getStoreAddressBasic())
+                .storeAddressDetail(request.getStoreAddressDetail())
+                .sellerName(request.getSellerName())
+                .storeTel(request.getStoreTel().replaceAll("-", ""))
+                .productCategory(request.getProductCategory())
+                .password(encodedPassword)
+                .signboard(request.getSignboard())
+                .storeNumber(request.getStoreNumber().replaceAll("-", ""))
+                .email(request.getEmail())
+                .isTaxRefundShop(request.getIsTaxRefundShop())
+                .build();
+        FranchiseeEntity save = franchiseeRepository.save(franchiseeEntity);
         franchiseeApplicantSaveService.save(franchiseeEntity);
+
+        UserPushTokenEntity userPushTokenEntity = UserPushTokenEntity.builder()
+            .userType(UserSelector.FRANCHISEE)
+            .userId(save.getId().toString())
+            .userToken(request.getPushToken())
+            .build();
+        userPushTokenService.save(userPushTokenEntity);
     }
 }
