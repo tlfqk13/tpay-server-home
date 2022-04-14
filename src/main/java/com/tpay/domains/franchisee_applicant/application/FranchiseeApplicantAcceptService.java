6c2a024a975e1b7c93541fccf4fefee16f7d63e2
sunba <nsg2318@naver.com>
2022-04-14 16:25:18 +0900

    add : 가맹신청 승인시 토픽 구독 및 푸시

--- src/main/java/com/tpay/domains/franchisee_applicant/application/FranchiseeApplicantAcceptService.java
+++ src/main/java/com/tpay/domains/franchisee_applicant/application/FranchiseeApplicantAcceptService.java
@@ -1,16 +1,29 @@
 package com.tpay.domains.franchisee_applicant.application;
 
 import com.tpay.commons.custom.CustomValue;
+import com.tpay.commons.exception.ExceptionState;
+import com.tpay.commons.exception.detail.InvalidParameterException;
+import com.tpay.commons.push.detail.PushTopic;
+import com.tpay.commons.util.UserSelector;
 import com.tpay.domains.franchisee.domain.FranchiseeEntity;
 import com.tpay.domains.franchisee_applicant.application.dto.FranchiseeFindRequest;
 import com.tpay.domains.franchisee_applicant.domain.FranchiseeApplicantEntity;
 import com.tpay.domains.franchisee_upload.application.FranchiseeBankFindService;
+import com.tpay.domains.push.application.PushNotificationService;
+import com.tpay.domains.push.application.TopicSubscribeService;
+import com.tpay.domains.push.application.dto.NotificationDto;
+import com.tpay.domains.push.domain.UserPushTokenEntity;
+import com.tpay.domains.push.domain.UserPushTokenRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import javax.transaction.Transactional;
+import java.util.List;
+
+import static com.tpay.commons.push.PushCategoryType.CASE_TWO;
+import static com.tpay.commons.push.PushType.TOKEN;
 
 @Service
 @RequiredArgsConstructor
@@ -19,6 +32,10 @@ public class FranchiseeApplicantAcceptService {
     private final FranchiseeApplicantFindService franchiseeApplicantFindService;
     private final FranchiseeBankFindService franchiseeBankFindService;
     private final WebClient.Builder builder;
+
+    private final TopicSubscribeService topicSubscribeService;
+    private final UserPushTokenRepository userPushTokenRepository;
+    private final PushNotificationService pushNotificationService;
 
     @Transactional
     public FranchiseeFindRequest accept(Long franchiseeApplicantIndex, FranchiseeFindRequest franchiseeFindRequest) {
@@ -29,7 +46,19 @@ public class FranchiseeApplicantAcceptService {
         franchiseeBankFindService.findByFranchiseeEntity(franchiseeEntity);
         FranchiseeFindRequest response = sendTo(franchiseeFindRequest);
         franchiseeApplicantEntity.accept();
+
         franchiseeEntity.memberInfo(response.getFranchiseeName(), response.getFranchiseeNumber());
+
+        //토픽 구독
+        UserPushTokenEntity userPushTokenEntity = userPushTokenRepository.findByUserIdAndUserType(franchiseeEntity.getId().toString(), UserSelector.FRANCHISEE)
+            .orElseThrow(() -> new InvalidParameterException(ExceptionState.INVALID_PARAMETER, "User Push Token Can't be null when Accepted"));
+        List<String> tokenList = List.of(userPushTokenEntity.getUserToken());
+        topicSubscribeService.subscribe(tokenList, PushTopic.FRANCHISEE);
+
+        //푸시 전송
+        NotificationDto.Request request = new NotificationDto.Request(CASE_TWO, TOKEN, userPushTokenEntity.getUserToken());
+        pushNotificationService.sendMessage(request);
+
         return response;
     }
 
--- src/main/java/com/tpay/domains/push/application/TopicSubscribeService.java
+++ src/main/java/com/tpay/domains/push/application/TopicSubscribeService.java
@@ -3,14 +3,26 @@ package com.tpay.domains.push.application;
 import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.TopicManagementResponse;
+import com.tpay.commons.push.detail.PushTopic;
 import com.tpay.domains.push.application.dto.TopicDto;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class TopicSubscribeService {
     public void subscribe(TopicDto.Request request) {
+        this.subscribeRequest(request.getTokens(), request.getPushTopic());
+    }
+
+
+    public void subscribe(List<String> token, PushTopic pushTopic) {
+        this.subscribeRequest(token, pushTopic);
+    }
+
+    public void subscribeRequest(List<String> token, PushTopic pushTopic) {
         try {
-            TopicManagementResponse response = FirebaseMessaging.getInstance().subscribeToTopic(request.getTokens(), request.getTopic());
+            TopicManagementResponse response = FirebaseMessaging.getInstance().subscribeToTopic(token, pushTopic.toString());
             System.out.println(response.getSuccessCount() + "token were subscribed successfully");
         } catch (FirebaseMessagingException e) {
             e.printStackTrace();
