87347d2ef3d867b5260bafeaf600626fb05dd093
sunba <nsg2318@naver.com>
2022-04-14 19:56:38 +0900

    mod : Topic 구독 제어 병합

--- src/main/java/com/tpay/domains/push/application/TopicSubscribeService.java
+++ src/main/java/com/tpay/domains/push/application/TopicSubscribeService.java
@@ -4,7 +4,6 @@ import com.google.firebase.messaging.FirebaseMessaging;
 import com.google.firebase.messaging.FirebaseMessagingException;
 import com.google.firebase.messaging.TopicManagementResponse;
 import com.tpay.commons.push.detail.PushTopic;
-import com.tpay.domains.push.application.dto.TopicDto;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,15 +11,7 @@ import java.util.List;
 @Service
 public class TopicSubscribeService {
 
-    public void subscribe(TopicDto.Request request) {
-        this.subscribeRequest(request.getTokens(), request.getPushTopic());
-    }
-
     public void subscribe(List<String> token, PushTopic pushTopic) {
-        this.subscribeRequest(token, pushTopic);
-    }
-
-    public void subscribeRequest(List<String> token, PushTopic pushTopic) {
         try {
             TopicManagementResponse response = FirebaseMessaging.getInstance().subscribeToTopic(token, pushTopic.toString());
             System.out.println(response.getSuccessCount() + "token were subscribed successfully");
@@ -28,4 +19,14 @@ public class TopicSubscribeService {
             e.printStackTrace();
         }
     }
+
+    public void unsubscribe(List<String> token, PushTopic pushTopic){
+        try {
+            TopicManagementResponse response = FirebaseMessaging.getInstance().unsubscribeFromTopic(token, pushTopic.toString());
+            System.out.println(response.getSuccessCount() + "token were unsubscribed successfully" );
+        } catch (FirebaseMessagingException e){
+            e.printStackTrace();
+        }
+    }
+
 }
