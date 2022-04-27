9c83a2f4e1f87fa3152f35bc3500659ffd108148
sunba <nsg2318@naver.com>
2022-04-27 11:30:27 +0900

    mod : 리팩토링 - BusinessNumberFindController 및 하위서비스 제거

--- src/main/java/com/tpay/domains/franchisee/application/dto/BusinessNumberFindRequest.java
+++
@@ -1,9 +0,0 @@
-package com.tpay.domains.franchisee.application.dto;
-
-import lombok.Getter;
-
-@Getter
-public class BusinessNumberFindRequest {
-    private String name;
-    private String phoneNumber;
-}
--- src/main/java/com/tpay/domains/franchisee/domain/FranchiseeRepository.java
+++ src/main/java/com/tpay/domains/franchisee/domain/FranchiseeRepository.java
@@ -7,10 +7,8 @@ import java.util.Optional;
 
 @Repository
 public interface FranchiseeRepository extends JpaRepository<FranchiseeEntity, Long> {
+
     boolean existsByBusinessNumber(String businessNumber);
 
     Optional<FranchiseeEntity> findByBusinessNumber(String businessNumber);
-
-    Optional<FranchiseeEntity> findBySellerNameAndStoreTel(String sellerName, String storeTel);
-
 }
