a980f46ddc4ba74f9e90c09644c5076557458acb
sunba <nsg2318@naver.com>
2022-06-16 15:44:21 +0900

    chore : 주석 - common - aria

--- src/main/java/com/tpay/commons/aria/PassportNumberDecryptService.java
+++ src/main/java/com/tpay/commons/aria/PassportNumberDecryptService.java
@@ -7,6 +7,12 @@ import org.springframework.stereotype.Service;
 
 import java.security.InvalidKeyException;
 
+
+/**
+ * 여권번호 복호화
+ * 초기 세팅 후 변경이력 없음. 관세청 여권번호 암호화 로직변경시에만 변경될 것으로 판단됨
+ * 현재는 사용되지 않는 클래스
+ */
 @Service
 @RequiredArgsConstructor
 public class PassportNumberDecryptService {
