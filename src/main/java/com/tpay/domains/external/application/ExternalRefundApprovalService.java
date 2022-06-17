2a97a5e44e50ff554c0a70cf4f5c9f85a9ad1a92
sunba <nsg2318@naver.com>
2022-06-17 15:18:31 +0900

    add : 외부환급 승인 수정 (에러코드 오타, 로거 추가)

--- src/main/java/com/tpay/domains/external/application/ExternalRefundApprovalService.java
+++ src/main/java/com/tpay/domains/external/application/ExternalRefundApprovalService.java
@@ -80,7 +80,7 @@ public class ExternalRefundApprovalService {
 
             //0000이 아닌경우 에러 발생
             if (!refundResponse.getResponseCode().equals("0000")) {
-                commonLogger.error1(externalRefundIndex, "R810X", "응답 코듣가 0이 아님. 응답메시지 : " + refundResponse.getMessage());
+                commonLogger.error1(externalRefundIndex, "R810X", "응답 코드가 0이 아님. 응답메시지 : " + refundResponse.getMessage());
                 orderService.deleteByIndex(orderEntity.getId());
                 return ExternalRefundResponse.builder().responseCode("R8102").message("[R8102] 시스템 에러입니다.").build();
             }
@@ -113,8 +113,6 @@ public class ExternalRefundApprovalService {
             commonLogger.error1(externalRefundApprovalRequest.getExternalRefundIndex(), "K8103", "인덱스 조회 실패");
             return ExternalRefundResponse.builder().responseCode("K8103").message("[K8103] 시스템 에러입니다.").build();
         } catch (InvalidParameterException e) {
-
-            // TODO: 2022/06/13 CustomException으로 생성
             commonLogger.error1(externalRefundApprovalRequest.getExternalRefundIndex(), "K8104", "어딘가에서 잘못된 파라미터");
             return ExternalRefundResponse.builder().responseCode("K8104").message("[K8104] 시스템 에러입니다.").build();
         } catch (IllegalArgumentException e) {
@@ -123,10 +121,12 @@ public class ExternalRefundApprovalService {
         } catch (WebfluxGeneralException e) {
             String code = e.getMessage().substring(0, 4);
             if (code.equals("2001")) {
-                commonLogger.error1(externalRefundApprovalRequest.getExternalRefundIndex(), "K8103", "통합한도 초과");
+                commonLogger.error1(externalRefundApprovalRequest.getExternalRefundIndex(), "R8103", "통합한도 초과");
                 return ExternalRefundResponse.builder().responseCode("R8103").message("[R8103] 즉시환급 통합한도(250만원) 초과자 입니다.").build();
+            } else {
+                commonLogger.error1(externalRefundApprovalRequest.getExternalRefundIndex(), "K8109", "관세청 응답코드 0 아님");
+                return ExternalRefundResponse.builder().responseCode("K8109").message("[K8109] 시스템 에러입니다.").build();
             }
-            return ExternalRefundResponse.builder().responseCode("K8108").message("[K8108] 시스템 에러입니다.").build();
         } catch (Exception e) {
             commonLogger.error1(externalRefundApprovalRequest.getExternalRefundIndex(), "K8106", "글로벌 에러. 응답메시지 : " + e.getMessage());
             return ExternalRefundResponse.builder().responseCode("K8106").message("[K8106] 시스템 에러입니다.").build();
