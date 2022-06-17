e013b7adc853a0cca9b731b986f9fb8cf5937f08
sunba <nsg2318@naver.com>
2022-06-17 13:57:47 +0900

    chore : 주석 - domains - customer 불필요 주석 삭제

--- src/main/java/com/tpay/domains/customer/application/CustomerSaveService.java
+++ src/main/java/com/tpay/domains/customer/application/CustomerSaveService.java
@@ -1,6 +1,5 @@
 package com.tpay.domains.customer.application;
 
-import com.tpay.commons.regex.RegExUtils;
 import com.tpay.domains.customer.domain.CustomerEntity;
 import com.tpay.domains.customer.domain.CustomerRepository;
 import lombok.RequiredArgsConstructor;
@@ -13,15 +12,9 @@ import javax.transaction.Transactional;
 public class CustomerSaveService {
 
     private final CustomerRepository customerRepository;
-    private final RegExUtils regExUtils;
 
     @Transactional
     public CustomerEntity saveByCustomerInfo(String customerName, String passportNumber, String nation) {
-
-        // TODO: 2022/02/09 아래 검증은 이미 암호화된 여권번호이므로 다른곳으로 옮겨야함 추후 개선할 것
-//    if(!regExUtils.validate(RegExType.PASSPORT,passportNumber)){
-//     throw new InvalidParameterException(ExceptionState.INVALID_PARAMETER,"Invalid passportNumber format");
-//    }
 
         CustomerEntity customerEntity =
             customerRepository.save(
