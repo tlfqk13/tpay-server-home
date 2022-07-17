89b61b24e9f07a7b4b06b8408f83a3a543c81456
sondong <fkffksk20@gmail.com>
2022-07-18 08:39:01 +0900

    CMS excel 파일 월생성

---
+++ src/main/java/com/tpay/domains/order/application/CmsCustomValue.java
@@ -0,0 +1,17 @@
+package com.tpay.domains.order.application;
+
+public class CmsCustomValue {
+    public static int TOPSECTION_STARTROW = 8;
+    public static int TOPSECTION_ENDROW = 14;
+    public static int TOPSECTION_FIRSTCELL = 1;
+    public static int TOPSECTION_SECONDCELL = 7;
+    public static int SECONDSECTION_ROW = 22;
+    public static int TOTALRESULT_ROW = 24;
+    public static int TOTALRESULT_FIRSTCELL = 3;
+    public static int TOTALRESULT_SECONDCELL = 7;
+    public static int SECONDSECTION_CELL = 0;
+    public static int DETAILRESULT_ROW = 30;
+    public static int DETAILRESULT_STARTCELL = 1;
+    public static int DETAILRESULT_ENDCELL = 7;
+
+}
