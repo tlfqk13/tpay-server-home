9032bc1ada27f28e2c12c676dda82f642e7d7fc6
brettahn <abu@successmode.kr>
2022-07-26 11:54:48 +0900

    add: push with topic
    + 푸시 알림이 토큰방식은 지연이 존재
    + TOPIC 방식은 실시간으로 전달된다해서 테스트 위해 추가

--- src/main/java/com/tpay/domains/push/application/NonBatchPushService.java
+++ src/main/java/com/tpay/domains/push/application/NonBatchPushService.java
@@ -3,13 +3,19 @@ package com.tpay.domains.push.application;
 
 import com.tpay.commons.push.PushCategoryType;
 import com.tpay.commons.push.PushType;
+import com.tpay.domains.franchisee.application.FranchiseeFindService;
+import com.tpay.domains.franchisee.domain.FranchiseeEntity;
 import com.tpay.domains.push.application.dto.NotificationDto;
+import com.tpay.domains.push.domain.SubscribeType;
+import com.tpay.domains.push.domain.TopicType;
 import com.tpay.domains.push.domain.UserPushTokenEntity;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
 import java.util.Optional;
 
 
@@ -23,6 +29,10 @@ public class NonBatchPushService {
 
     private final UserPushTokenService userPushTokenService;
     private final PushNotificationService pushNotificationService;
+
+    private final TopicSubscribeService topicSubscribeService;
+
+    private final FranchiseeFindService franchiseeFindService;
 
     // 토큰기반 Non-Batch 푸시 알람
     @Transactional
@@ -57,6 +67,23 @@ public class NonBatchPushService {
                 request = request.setFrontBody(message);
         }
         pushNotificationService.sendMessageByToken(request);
+    }
+
+    // Topic 기반 푸시 알림
+    @Transactional
+    public void nonBatchPushWithTopic(PushCategoryType pushCategoryType, Long franchiseeIndex) {
+        log.trace("nonBatchPushWithTopic Start");
+        FranchiseeEntity entity = franchiseeFindService.findByIndex(franchiseeIndex);
+        TopicType topic = TopicType.FRANCHISEE;
+
+        topicSubscribeService.subscribeByFranchisee(List.of(entity), topic, SubscribeType.SUBSCRIBE);
+        NotificationDto.Request request = new NotificationDto.Request(pushCategoryType, PushType.TOPIC, topic.toString());
+        String send = pushNotificationService.sendMessageByTopic(request);
+        topicSubscribeService.subscribeByFranchisee(List.of(entity), topic, SubscribeType.UNSUBSCRIBE);
+
+//        subscribeList.stream().map(userPushTokenService::findByToken).forEach(e -> pushHistoryService.saveHistory(request, send, entity.get(), 0L));
+//        System.out.println("[" + LocalDateTime.now() + "] Pushed - " + pushCategoryType);
+        log.trace("nonBatchPushWithTopic finish [{}] : [{}] ", LocalDateTime.now(), send);
     }
 
 }
