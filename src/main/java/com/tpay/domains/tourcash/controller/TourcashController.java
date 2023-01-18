6d38c8fa2a10fddc7462bc8134cfb59310c27914
brettahn <abu@successmode.kr>
2023-01-18 15:09:23 +0900

    add: 서버 배포 파일 버저닝 작업, 투어캐시 한도조회 컨트롤러 추가, 에러 상태 정리
    + 투어캐시 한도조회 컨트롤러 추가
    + 에러 상태 정리
    + 서버 배포 파일 버저닝
      + build.gradle 내 배포 버전 설정
      + 서버 종료에서 활용할 pid 저장
        + main 내 PidWriter
        + yml 내 pid 기입될 파일명 설정
      + VERSION.md
        + 버전 로그 및 작성법

--- src/main/java/com/tpay/domains/tourcash/controller/TourcashController.java
+++ src/main/java/com/tpay/domains/tourcash/controller/TourcashController.java
@@ -4,16 +4,15 @@ import com.tpay.commons.exception.ExceptionState;
 import com.tpay.commons.exception.detail.FranchiseeAuthenticationException;
 import com.tpay.commons.util.IndexInfo;
 import com.tpay.commons.util.UserSelector;
+import com.tpay.domains.refund_core.application.LimitFindService;
 import com.tpay.domains.refund_core.application.RefundApproveService;
+import com.tpay.domains.refund_core.application.dto.RefundLimitRequest;
 import com.tpay.domains.refund_core.application.dto.RefundResponse;
 import com.tpay.domains.tourcash.dto.TourcashRefundApproveDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @Slf4j
 @RequestMapping("/tourcash")
@@ -22,9 +21,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class TourcashController {
 
     private final RefundApproveService refundApproveService;
+    private final LimitFindService limitFindService;
+
+    @GetMapping("/refund/limit")
+    public ResponseEntity<RefundResponse> limit(
+            @RequestBody RefundLimitRequest request) {
+        RefundResponse response = limitFindService.find(request);
+        return ResponseEntity.ok(response);
+    }
 
     @PostMapping("/refund/approval")
     public ResponseEntity<RefundResponse> approve(@RequestBody TourcashRefundApproveDto dto) {
+
         if (dto.getFranchiseeIndex() != 95L) {
             throw new FranchiseeAuthenticationException(
                     ExceptionState.AUTHENTICATION_FAILED, "Token not exists");
